348c7356487ba1781ad6b12cd03bf46337a9e8ef
Thando Mkhonza <Donatello9503@gmail.com>
2024-07-23 02:16:57 +0200

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Flip from "./Flip";
 import './hero.css';
 
 const Hero = () => {
+  const words = ["free", "unlimited", "quality"];
   return (
     <section className="hero-section">
       <div className="hero-content">
-        <h1>Free Image Converter</h1>
+        <h1><Flip words={words} /> Image Converter</h1>
         <p>Convert images in bulk without losing their quality. It’s fast, free & easy-to-use!</p>
       </div>
     </section>
